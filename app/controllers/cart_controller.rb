24a41f0aa4e6f96f269e096164ac1626e992bd44
Guillaume REYGNER <74305277+guillaume-rygn@users.noreply.github.com>
2022-03-15 17:49:52 +0100

    link item#index + maj cart controller (#11)

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,24 +1,27 @@
 class CartController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user
 
     def create
-    @cart = JoinTableCartItem.new(cart_params)
+    @cartitem = JoinTableCartItem.new(
+      'item_id' => params[:item_id],
+      'quantity' => params[:quantity],
+      'cart_id' => current_user.id
+    )
 
-    if @cart.save
-      flash[:notice] = "add to cart"
+    if @cartitem.save
+      flash[:notice] = "Item Add"
     else
-      flash[:notice] = "Please try again"
+      flash[:notice] = "please try again"
     end
-    puts"$"*650
-    puts params.inspect
+  
     respond_to do |format|
-        format.html { redirect_to cart_path(params[:cart_id]) }
+        format.html { redirect_to cart_path(Cart.find_by(user_id: current_user.id).id) }
         #format.js { }
       end
   end
 
   def show
-    @cart = Cart.find(params[:id])
+    @cart = Cart.find_by(user_id: params[:user_id])
     
   end
 
@@ -32,8 +35,12 @@ class CartController < ApplicationController
 
   private
 
-  def cart_params
-    params.permit(:item_id, :quantity, :cart_id)
+  def authenticate_user
+    unless current_user
+      flash[:danger] = "Please log in."
+      redirect_to new_user_session_path
+    end
   end
+
 
 end
